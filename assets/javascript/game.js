fddaefd4d59668ba49d892af53bb9bc5200f7088
JenPhD <jpemberton76@gmail.com>
2016-07-14 10:43:40 -0400

    initial commit

---
+++ assets/javascript/game.js
@@ -0,0 +1,9 @@
+//prompt to get a letter from the user
+
+//var userGuess = prompt("letter guess");
+
+   // Sets the computer choices 
+//var computerChoices = ['WASHINGTON', 'ADAMS', 'JEFFERSON', 'MADISON', 'MONROE', 'JACKSON', 'VANBUREN', 'HARRISON', 'TYLER', 'POLK', 'TAYLOR', 'FILLMORE', 'PIERCE', 'BUCHANAN', 'LINCOLN', 'JOHNSON', 'GRANT', 'HAYES', 'GARFIELD', 'ARTHUR', 'CLEVELAND', 'HARRISON', 'MCKINLEY', 'ROOSEVELT', 'TAFT', 'WILSON', 'HARDING', 'COOLIDGE', 'HOOVER', 'TRUMAN', 'EISENHOWER', 'KENNEDY', 'NIXON', 'FORD', 'CARTER', 'REAGAN', 'BUSH', 'CLINTON', 'OBAMA'];
+
+// Declares the tallies to 0 
+//
