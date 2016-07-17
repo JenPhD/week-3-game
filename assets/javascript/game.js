ede45fc399c694da90568a7e033da94b1e3eabeb
JenPhD <jpemberton76@gmail.com>
2016-07-16 23:28:29 -0400

    more improvements

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -63,6 +63,9 @@ winTest: function () {
 //Update the word and spaces if there is a win, loss, or right guess
 wordUpdate: function () {
   var wordArray = "";
+  //for (var i = 0; i < computerAnswer.length; i++) {
+           // displayAnswer += blanks[i] + " ";
+        //}
       console.log(wordArray);
       document.getElementById('word').innerHTML = 'Word: ' + wordArray;
       },
@@ -71,38 +74,19 @@ wordUpdate: function () {
   resetGame: function () {
     wrongLetter = 7;
     document.getElementById('guessesLeft').innerHTML = "Number of guesses left: " + wrong;
-    word = game.presidents[Math.floor(Math.random() * game.presidents.length)];
+    var word = game.presidents[Math.floor(Math.random() * game.presidents.length)];
     console.log(word);
     var wrongArray = [];
     var wordArray = "";
     document.getElementById('wrong').innerHTML = "Wrong letters" + wrongArray;
+    for (var i = 0; i < word.length; i++) {
+            wordArray[i] = "_";
+        console.log(this.wordArray);
+        document.getElementById("word").innerHTML = "Word: " + wordArray;
+      }
+      game.wordUpdate();
     },
   	      
-//Check userGuess to see if it is in the word.
-	
-	checkLetter: function() {
-	var indexLetter = word.indexOf('userGuess');
-	if(wrongArray.indexOf('userGuess') > -1)
-    {//do nothing
-
-    }
-
-    else if(indexLetter == -1) {
-      alert: 'Try again!';
-       wrong--;
-       console.log(wrong);
-       wrongArray.push(userGuess);
-       console.log(wrongArray);
-   		}
-
-   	else if(indexLetter > -1) {
-   		alert: 'Yay, you got a letter!'
-   		wordArray = wordArray.splice(indexLetter, 1, userGuess);
-   		console.log(wordArray);
-   		right++;
-   		console.log(right);
-   		}
-	}
 }
 
 //Beginning counts;
@@ -112,18 +96,59 @@ document.getElementById('guessesLeft').innerHTML = "Number of Guesses Left: " + 
 document.getElementById('wrong').innerHTML = "Wrong letters: " + wrongArray;
 
 	
-//game setup and procedures
-	
-	
-//UserGuess letter
-	document.onkeyup = function(event) {
-		var userGuess = String.fromCharCode(event.keyCode).toUpperCase();
-		console.log(userGuess);
-    game.winTest();
-    game.wordUpdate();
-    game.resetGame();
+//// Randomly selecting which answer the computer will use
+var word = game.presidents
+[Math.floor(Math.random() * game.presidents.length)].toUpperCase();
+// console.log(computerAnswer);
 
-	}
+var wordArray = [];
+var wrongArray = [];
+
+//create blank array for displaying
+for (var i = 0; i < word.length; i++) {
+      wordArray[i] = "_";
+      console.log(this.wordArray);
+      document.getElementById("word").innerHTML = "Word: " + wordArray;
+      },
+
+game.wordUpdate();
+
+//Check userGuess to see if it is in the word.
+document.onkeyup = function(event) {
+  var userGuess = String.fromCharCode(event.keyCode);
+  checkLetter: function() {
+  var indexLetter = word.indexOf('userGuess');
+    if(wrongArray.indexOf('userGuess') > -1)
+    {//do nothing
+
+    }
+    else if((indexLetter == -1) && (wrong > 0)) {
+      alert: 'Try again!';
+       wrong--;
+       console.log(wrong);
+       document.getElementById('guessesLeft').innerHTML = "Number of Guesses Left: " + wrong;
+       wrongArray.push(userGuess);
+       document.getElementById('wrong').innerHTML = "Wrong letters: " + wrongArray;
+       console.log(wrongArray);
+      }
+
+    else if ((indexLetter > -1 ) && (right < word.length)) {
+      alert: 'Yay, you got a letter!'
+      wordArray = wordArray.splice(indexLetter, 1, userGuess);
+      console.log(wordArray);
+      right++;
+      console.log(right);
+      game.wordUpdate();
+      }
+
+    else ((indexLetter == -1) && (wrong = 0)) {
+      alert: 'You lose :(';
+      losses++;
+      game.resetGame();
+    }
+game.winTest ();
+  }
+    
 
 
 	
